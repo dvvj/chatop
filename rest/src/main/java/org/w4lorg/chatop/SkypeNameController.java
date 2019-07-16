f2a370847df7ab03ad796a932bc3cf1f5ce44583
devvj <devvjj5@gmail.com>
2019-07-16 15:18:32 +0200

    sid 2 skype names

--- rest/src/main/java/org/w4lorg/chatop/SkypeNameController.java
+++ rest/src/main/java/org/w4lorg/chatop/SkypeNameController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -32,6 +30,38 @@ public class SkypeNameController {
     return uids.stream().map(uid2SkypeNameMap::get)
       .filter(x -> x != null)
       .collect(Collectors.toList());
+  }
+
+  private static Map<String, List<UidSkypeNamePair>> generateSid2SkypeNamesMap() {
+    Map<String, List<UidSkypeNamePair>> res = new HashMap<>();
+
+    res.put(
+      "sid01",
+      Arrays.asList(
+        new UidSkypeNamePair("uid001", "skype-name-001"),
+        new UidSkypeNamePair("uid002", "skype-name-002")
+      )
+    );
+
+    res.put(
+      "sid02",
+      Arrays.asList(
+        new UidSkypeNamePair("uid001", "skype-name-001"),
+        new UidSkypeNamePair("uid003", "skype-name-003"),
+        new UidSkypeNamePair("uid004", "skype-name-004")
+      )
+    );
+    return res;
+  }
+
+  private final static Map<String, List<UidSkypeNamePair>> sid2SkypeNameMap = generateSid2SkypeNamesMap();
+
+  @PostMapping(path="/sid2SkypeNames")
+  public List<UidSkypeNamePair> sid2SkypeNames(
+    @RequestBody String sid
+  ) {
+    List<UidSkypeNamePair> res = sid2SkypeNameMap.get(sid);
+    return res != null ? res : new ArrayList<>(0);
   }
 
 }
